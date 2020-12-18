00ef5b54d147fcace1cd63bf13fe3a3a4d4c7cab
CedricJGit <cedric.jeannot@free.fr>
2020-12-18 14:12:21 +0100

    pb  de post doublon

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 // Chargement du DOM quand bien terminé (Fonction anonyme)
-window.addEventListener('load', function(evt) {
+addEventListener('load', function(evt) {
     initialisationJS('Thomas Pesquet');
 
+    // Accrochage d'un écouteur d'event sur une balise
     document.querySelector('form').addEventListener('submit', formSubmited);
 
     // Chargement initial des post-it
@@ -16,41 +17,48 @@ window.addEventListener('load', function(evt) {
     });
 });
 
-function initialisationJS(_sPrenom) {
+function initialisationJS(sPrenom) {
 
     let msg = document.querySelector('#jsload');
     msg.style.backgroundColor = 'green';
     // msg.innerText ='JS Ok'
-    msg.innerHTML ='Le <span style="font-weight:900"> JS</span> est chargé pour '+_sPrenom
+    msg.innerHTML ='Le <span style="font-weight:900"> JS</span> est chargé pour '+sPrenom
 };
 
 function formSubmited(evt) {
     evt.preventDefault();   // Pour ne pas recharger la page
     console.log('Le formulaire est validé');
 
-    console.log(evt.target[0].value);   // Titre
-    console.log(evt.target[1].value);   // Date
-    console.log(evt.target[2].value);   // Heure
-    console.log(evt.target[3].value);   // Commentaire
-
     let myForm = document.forms['editor-form']  // Autre manière de récupérer les valeurs
-    //let dateFormat = moment(myForm['date'].value, 'DD MM YYYY')
-    createPostit(myForm['title'].value, evt.target[1].value, evt.target[2].value, evt.target[3].value)
+    
+    // Construction de l'objet à envoyer au rest
+    let postit = { titre:myForm["title"].value, 
+                    datetime:myForm["date"].value+"T"+myForm["time"].value,
+                    description:myForm["description"].value
+                };
+    console.log(postit);
+
+    // Appel rest pour l'ajout dans la liste et recup de l'id
+    (new Crud(BASE_URL)).creer('/postit', postit, function(objSaved) {
+        createPostitByObject(objSaved);
+    });
+    
+    // createPostit(myForm['title'].value, evt.target[1].value, evt.target[2].value, evt.target[3].value)
 };
 
 // Nouveau post-it
-function createPostit(_sTitre, _sDate, sHeure, _sComment) {
+function createPostit(sTitre, sDate, sHeure, sComment) {
     let postit = document.createElement('div');
     
     postit.classList.add('postit');  
 
     postit.innerHTML = 
-        '<div class="postit-titre">'+_sTitre+'</div>\
+        '<div class="postit-titre">'+sTitre+'</div>\
         <br>\
         Date et heure : \
-        <span class="datetime">'+_sDate+'</span>\
+        <span class="datetime">'+sDate+'</span>\
         <span class="datetime">'+sHeure+'</span>\
-        <h2>Mode d emploi :</h2>'+_sComment+'\
+        <h2>Description :</h2>'+sComment+'\
         <div>\
             <input type="button" value="Supprimer" img="img/suppr" class="suppr-postit">\
         </div>\
@@ -81,11 +89,12 @@ function createPostitByObject(postitInput) {
         '<div class="postit-titre">'+postitInput.titre+'</div>\
         <br>\
         Date et heure : \
-        <span class="datetime">'+postitInput.datetime.substring(0,10)+'</span>\
-        <span class="datetime">'+postitInput.datetime.substring(11)+'</span>\
-        <h2>Mode d emploi :</h2>'+postitInput.description+'\
+        <span class="datetime postit-date">'+postitInput.datetime.substring(0,10)+'</span>\
+        <span class="datetime postit-heure">'+postitInput.datetime.substring(11)+'</span>\
+        <h2>Mode d emploi :</h2>\
+        <div class="postit-descr">'+postitInput.description+'</div>\
         <div class="suppr-postit" style="float: right;">\
-        <img src="img/suppr.png"/>\
+            <img src="img/suppr.png"/>\
         </div>';
 
     let liste = document.querySelector('#list');
@@ -93,11 +102,43 @@ function createPostitByObject(postitInput) {
 
     let close = postit.querySelector('.suppr-postit img');
     close.addEventListener('click', supprPostit);
+
+    postit.addEventListener('dblclick', majSelectedPostit);
 };
 
 
 // Suppression du post-it
 function supprPostit(evt) {
+    evt.stopPropagation(); 
+
     console.log('Suppression post-it',evt);
-    evt.currentTarget.parentElement.parentElement.remove();
-}
+
+    var elem = evt.currentTarget.parentElement.parentElement;
+    let domPostitId = elem.id.substring(7);
+    (new Crud(BASE_URL)).suppr('/postit/'+domPostitId, function() {
+        //console.log('Suppression post-it', evt.path[2]);
+        //evt.path[2].remove();
+        elem.remove();
+    });
+};
+
+
+function majSelectedPostit(evt){
+    console.log('j\'double-cliqué sur un post-it', evt);
+
+    // Récupérer la balise postit
+    var domPostitId = evt.currentTarget;
+    console.log(
+        domPostitId.id.substring(7),
+        domPostitId.querySelector('.postit-titre').innerText,
+        domPostitId.querySelector('.postit-date').innerText,
+        domPostitId.querySelector('.postit-heure').innerText,
+        domPostitId.querySelector('.postit-descr').innerText
+    );
+    
+    document.forms["editor-form"]["id"].value = domPostitId.id.substring(7);
+    document.forms["editor-form"]["title"].value = postit.querySelector('.postit-titre').innerText;
+    document.forms["editor-form"]["date"].value = postit.querySelector('.postit-date').innerText;
+    document.forms["editor-form"]["time"].value = postit.querySelector('.postit-heure').innerText;
+    document.forms["editor-form"]["descr"].value = postit.querySelector('.postit-descr').innerText;
+};
--- js/rest.js
+++ js/rest.js
@@ -3,12 +3,12 @@ var BASE_URL = 'http://localhost:3131';
 // Objet permettant les appels http
 let Crud = function (baseurl) {
 
-    function get(_sRessourceUrl, callback) {
+    function _get(sRessourceUrl, callback) {
         // instanciation de xhr
         let xhr=new XMLHttpRequest();
         
         // Ouverture de la connexion
-        xhr.open('GET', baseurl+_sRessourceUrl);
+        xhr.open('GET', baseurl+sRessourceUrl);
         
         // Tâche à effectuer à chaque changement de readystate (passage d'une étape de réception)
         // 1 => Open, 2=> Send, 3=> En cours de réception, 4=> Fin de réception
@@ -16,7 +16,6 @@ let Crud = function (baseurl) {
             if(evt.currentTarget.readyState < XMLHttpRequest.DONE){return;}
             var objt = JSON.parse(evt.currentTarget.response);
             console.log(objt);
-
             
             callback(objt);
         };
@@ -27,44 +26,46 @@ let Crud = function (baseurl) {
 
     /**
      * Création d'une ressource sur _sRessourceUrl
-     * @param {uri} _sRessourceUrl chemin du post
-     * @param {object} _sRessource data à envoyer
+     * @param {uri} sRessourceUrl chemin du post
+     * @param {object} sRessource data à envoyer
+     * @param {function} callback Fonction de callback avec injection de retour
      */
-    function post(_sRessourceUrl, _sRessource){
-        // instanciation de xhr
+    function _post(sRessourceUrl, sRessource, callback){
+        // Instanciation de xhr
         let xhr=new XMLHttpRequest();
         
         // Ouverture de la connexion
-        xhr.open('POST', baseurl+_sRessourceUrl);
+        xhr.open('POST', baseurl + sRessourceUrl);
         
         // Spécification... 
         xhr.setRequestHeader('Content-Type','application/json');    // ...du type de contenu
         xhr.setRequestHeader('Accept','application/json');          // ... de ce qui est attendu en retour
 
         xhr.onreadystatechange = function(evt){
-            if(xhr.readyState < 4){return;}
-            
+            if (xhr.readyState < 4 || xhr.status != 201) { return; }
             console.log(JSON.parse(xhr.response));
+            callback(JSON.parse(xhr.response));
         };
 
         // Envoi de la requête avec transformation en JSON du contenu objet
-        xhr.send(JSON.stringify(_sRessource)); 
+        xhr.send(JSON.stringify(sRessource)); 
     }
 
     /**
      * Suppression d'une ressource sur _sRessourceUrl 
-     * @param {uri} _sRessourceUrl 
+     * @param {uri} sRessourceUrl 
      */
-    function remove(_sRessourceUrl){
+    function _remove(sRessourceUrl, callback){
         // instanciation de xhr
         let xhr=new XMLHttpRequest();
         
         // Ouverture de la connexion
-        xhr.open('DELETE', baseurl+_sRessourceUrl);
+        xhr.open('DELETE', baseurl+sRessourceUrl);
 
-        xhr.onreadystatechange = function(evt){
-            if(xhr.readyState < 4){return;}
-            console.log(JSON.parse(xhr.response));
+        xhr.onreadystatechange = function(evt) {
+            if(xhr.readyState < 4 || xhr.status != 200) { return; }
+            //console.log(JSON.parse(xhr.response));
+            callback();
         };
         
         // Envoi de la requête
@@ -74,15 +75,15 @@ let Crud = function (baseurl) {
 
     /**
      * Mise à jour d'une ressource sur _sRessourceUrl
-     * @param {uri} _sRessourceUrl chemin du post
-     * @param {object} _sRessource data à envoyer
+     * @param {uri} sRessourceUrl chemin du post
+     * @param {object} sRessource data à envoyer
      */
-    function put(_sRessourceUrl, _sRessource){
+    function _put(sRessourceUrl, sRessource){
         // instanciation de xhr
         let xhr=new XMLHttpRequest();
         
         // Ouverture de la connexion
-        xhr.open('PUT', baseurl+_sRessourceUrl);
+        xhr.open('PUT', baseurl+sRessourceUrl);
         
         // Spécification... 
         xhr.setRequestHeader('Content-Type','application/json');    // ...du type de contenu
@@ -95,14 +96,14 @@ let Crud = function (baseurl) {
         };
 
         // Envoi de la requête avec transformation en JSON du contenu objet
-        xhr.send(JSON.stringify(_sRessource)); 
+        xhr.send(JSON.stringify(sRessource)); 
     }
 
     // pour les accès extérieurs de l'instance de ces fonctions
-    this.recup=get;
-    this.creer=post;
-    this.maj=put;
-    this.suppr=remove;
+    this.recup=_get;
+    this.creer=_post;
+    this.maj=_put;
+    this.suppr=_remove;
 };
 
 //let crud = new Crud(BASE_URL);
